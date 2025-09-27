7e819fceb5c748ee604a38e25bfaa1def4b7396d
Walex Brendo Rodrigues dos Santos <202412170022@ifto.local>
2025-09-26 22:02:29 -0300

    atualização de teçlas

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function mensagem (txt) {
+    alert (txt);
+
+}
+
+   function select (e){
+        let nome= document.getElementById ("nome");
+        nome = nome.value;
+
+        let input = document.getElementById("input");
+        input.value = nome
+    }
